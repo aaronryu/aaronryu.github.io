3a2fb5c14c7a40c854ac5ea8321081bc657f3aa2
Aaron Ryu <fbcndah@gmail.com>
2021-11-25 01:50:34 +0900

    [B-000] init & porting: dynamic post loader API

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,7 @@
-exports.createPages = async ({ actions }) => {
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  // console.log(actions)
+  // console.log(graphql)
+  // console.log(reporter)
   const { createPage } = actions
   createPage({
     path: "/using-dsg",
@@ -6,4 +9,34 @@ exports.createPages = async ({ actions }) => {
     context: {},
     defer: true,
   })
+
+  const [ filePostedEdged ] = await Promise.all([ getFilePostedData(graphql, reporter) ])
+  console.log(filePostedEdged)
+  filePostedEdged.forEach(childMdx => {
+    createPage({
+      path: childMdx.slug + '/',
+      component: `${__dirname}/src/templates/post.tsx`,
+      context: {
+        slug: `${childMdx.slug}`,
+      }
+    })
+  })
+}
+
+async function getFilePostedData(graphql, reporter) {
+  const postsResult = await graphql(`
+    query {
+      allMdx {
+        nodes {
+          slug
+        }
+      }
+    }
+  `)
+
+  if (postsResult.errors) {
+    reporter.panic('failed to create posts', postsResult.errors)
+  }
+
+  return postsResult.data.allMdx.nodes
 }
