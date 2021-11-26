72057d5be9e9ecf889c630cde47e20fd04c9e8bd
Aaron Ryu <fbcndah@gmail.com>
2021-11-27 03:19:14 +0900

    [B-000] init: use emotion and fix - Property 'css' does not exist

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,13 +31,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `aaron.ryu`,
+        short_name: `aaron`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#29C785`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        theme_color: `#29C785`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
