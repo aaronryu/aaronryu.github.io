1f0a7945e32aa1abbc5eb4d145908ab4796290cf
Aaron Ryu <fbcndah@gmail.com>
2021-12-26 21:46:00 +0900

    [B-000] init: Right TOC Bar, dynamic rendering - currentHref, design

--- src/templates/post/index.tsx
+++ src/templates/post/index.tsx
@@ -1,9 +1,10 @@
 import { graphql } from 'gatsby'
 import { css } from '@emotion/react'
 import useSiteMetadata from '../../hooks/use-sitemetadata'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Article, { GatsbyImageSharpFluidWithWebp } from './post-article'
 import PostSeo from './post-seo'
+import { useScroll } from './scroll'
 
 export interface PostPreview {
   slug: string
@@ -53,6 +54,7 @@ interface Props {
   }
 }
 
+const HEADER_OFFSET_Y = 76
 const PostTemplate: React.FunctionComponent<Props> = ({
   data: { mdx },
 }) => {
@@ -61,7 +63,41 @@ const PostTemplate: React.FunctionComponent<Props> = ({
   const imageSrc = frontmatter.image
     ? frontmatter.image.childImageSharp.fluid.srcWebp
     : ''
-  console.log(toc)
+  const [currentHeaderUrl, setCurrentHeaderUrl] = useState<string>('')
+  const { scrollY } = useScroll();
+
+  const handleScroll = (currentOffsetY: number) => {
+    let aboveHeaderUrl // 화면 바로 위쪽에 위치한 header
+    const headerElements: NodeListOf<Element> = document.querySelectorAll('.anchor-header')
+    for (const elem of Array.from(headerElements)) {
+      const { top } = elem.getBoundingClientRect()
+      const elemTop = top + currentOffsetY
+      const isLast = elem === headerElements[headerElements.length - 1]
+      if (currentOffsetY < elemTop - HEADER_OFFSET_Y) {
+        // 기억해둔 aboveHeaderUrl이 있다면 바로 위 header와 현재 element 사이에 화면이 스크롤 되어 있다.
+        aboveHeaderUrl &&
+          setCurrentHeaderUrl(aboveHeaderUrl)
+        // 기억해둔 aboveHeaderUrl이 없다면 첫번째 header다. 
+        // 이때는 어떤 header도 active 하지 않은 상태다.
+        !aboveHeaderUrl && setCurrentHeaderUrl('')
+        break;
+      } else {
+        // 마지막 header면 다음 element가 없기 때문에 현재 header를 active header라 간주한다.
+        const href = elem.getAttribute('href')
+        if (href) {
+          if (isLast)
+            setCurrentHeaderUrl(decodeURI(href))
+          else
+            aboveHeaderUrl = decodeURI(href)
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    handleScroll(scrollY)
+  }, [scrollY])
+
   return (
     <div css={styles.container}>
       <PostSeo
@@ -77,7 +113,7 @@ const PostTemplate: React.FunctionComponent<Props> = ({
       <TableOfContents
         toc={toc}
         depth={4}
-        currentHeaderUrl={'#프리-커밋-훅pre-commit-hook'}
+        currentHeaderUrl={currentHeaderUrl}
       />
       <Article
         siteUrl={siteUrl}
@@ -102,55 +138,58 @@ const PostTemplate: React.FunctionComponent<Props> = ({
 const makeTocHeaderHtml = (each: TocHeader, remainDepth: number) => {
   if (each.items !== undefined) {
     return (
-      <React.Fragment>
+      <React.Fragment key={each.title}>
         <li><a href={each.url}>{each.title}</a></li>
         <ul>{makeTocHeadersHtml(each.items, (remainDepth - 1))}</ul>
       </React.Fragment>
     );
   } else {
-    return <li><a href={each.url}>{each.title}</a></li>;
+    return <li key={each.title}><a href={each.url}>{each.title}</a></li>;
   }
 }
 
 const makeTocHeadersHtml = (items: Array<TocHeader>, depth: number) => {
-  return <React.Fragment>{(depth > 0) && items.map((each) => makeTocHeaderHtml(each, depth))}</React.Fragment>
+  return <React.Fragment key={depth}>{(depth > 0) && items.map((each) => makeTocHeaderHtml(each, depth))}</React.Fragment>
+}
+
+const makeToc = (toc: TocHeaders, depth: number) => {
+  return <ul>{makeTocHeadersHtml(toc.items, depth)}</ul>
 }
 
 const TableOfContents = ({ toc, depth, currentHeaderUrl }: { toc: TocHeaders, depth: number, currentHeaderUrl: string }) => {
-  const styledItemsHtml = <ul css={{
-    // 낮은 depth가 더 안쪽으로 들어가도록 모든 ul에 marginLeft를 부여한다.
-    '& ul': {
-      marginLeft: '-1rem', 
-    },
-    // currentHeaderUrl 문자열이 href 속성에 포함된다면 아래 스타일을 부여한다. 
-    // 현재 스크롤에 해당하는 Header를 하이라이트 하기 위함
-    [`& ul > li a[href*="${currentHeaderUrl}"]`]: { 
-      fontSize: '15px',
-      color: '#333333',
-      fontWeight: '600',
-    },
-    fontSize: '0.9rem',
-    fontWeight: 500,
-  }}>{makeTocHeadersHtml(toc.items, depth)}</ul>
   return (
     <div
-      css={{
-        position: 'fixed',
-        top: 100,
-        right: 0,
-        width: 400,
-        height: 220,
-        backgroundColor: 'grey',
-        '@media screen and (calc((100vw - 720px) / 2 - 50px))': {
-          display: 'none',
-          '@media screen and (min-width: 1200px)': {
-            display: 'block',
-            fontSize: '14px',
-          },
+    css={{
+      // 낮은 depth가 더 안쪽으로 들어가도록 모든 ul에 marginLeft를 부여한다.
+      '& ul': {
+        marginLeft: '-1rem', 
+      },
+      // currentHeaderUrl 문자열이 href 속성에 포함된다면 아래 스타일을 부여한다. 
+      // 현재 스크롤에 해당하는 Header를 하이라이트 하기 위함
+      [`& ul > li a[href*="${currentHeaderUrl}"]`]: { 
+        color: 'var(--text0)',
+        fontWeight: '600',
+      },
+      fontSize: '0.9rem',
+      fontWeight: 500,
+      //
+      position: 'fixed',
+      top: 100,
+      right: 0,
+      width: 400,
+      height: 220,
+      margin: '1rem 0',
+      borderLeft: '1px solid var(--hr)',
+      '@media screen and (calc((100vw - 720px) / 2 - 50px))': {
+        display: 'none',
+        '@media screen and (min-width: 1200px)': {
+          display: 'block',
+          fontSize: '14px',
         },
-      }}
+      },
+    }}
     >
-      {styledItemsHtml}
+      {makeToc(toc, depth)}
     </div>
   )
 }
