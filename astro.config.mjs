55fec6349c324ed1810f49acc7e8dcb799abe798
Aaron Ryu <fbcndah@gmail.com>
2026-04-07 01:20:47 +0900

    [Feat] 상대경로가 아닌 절대경로 import 위한 여러 설정들을 완수한 후 Header 연결

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // (A) Astro-specific options
+  // site: 'https://aaronryu.github.io', - Sitemap / Canonocal 생성하여 SEO 가속
+  //
+  // (B) Vite-specific options
+  // vite: { ... },
+});
