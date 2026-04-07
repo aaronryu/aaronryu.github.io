015bad661c89cc874622db7f3e3f66adb38a4064
Aaron Ryu <fbcndah@gmail.com>
2026-04-07 19:38:50 +0900

    [Feat] SEO 관점 및 구독자들에게 내 새 글을 서빙하고자한다면 RSS 적용은 필수

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   // (A) Astro-specific options
   // site: 'https://aaronryu.github.io', - Sitemap / Canonocal 생성하여 SEO 가속
+  site: "https://aaronryu.netlify.app",
   //
   // (B) Vite-specific options
   // vite: { ... },
