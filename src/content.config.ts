fab75a2f7c4d8a262cce63b334fa1886b7dd7c67
Aaron Ryu <fbcndah@gmail.com>
2026-04-08 00:26:20 +0900

    [Feat] 기존 Gatsby 에서 사용했던 마크다운 블로그 글 적용

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,17 +6,20 @@ import { defineCollection } from "astro:content";
 import { z } from "astro/zod";
 // 각 컬렉션에 대한 `loader` 및 `schema` 정의
 const blog = defineCollection({
-  loader: glob({ pattern: "**/[^_]*.md", base: "./src/blog" }),
+  loader: glob({ pattern: "**/[^_]*.md", base: "./blog" }),
   schema: z.object({
     title: z.string(),
-    pubDate: z.date(),
-    description: z.string(),
-    author: z.string(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
-    tags: z.array(z.string()),
+    category: z.array(z.string()).min(1), // 태그의 역할도 겸하지만 최상위 태그 기준으로 글을 관심사별로 정리하여 조회할 수 있도록
+    created: z.date(),
+    updated: z.date().optional(),
+    deck: z.string(),
+    abstract: z.string(),
+    image: z
+      .object({
+        url: z.string(),
+        alt: z.string(),
+      })
+      .optional(),
   }),
 });
 // 단일 `collections` 객체를 내보내 컬렉션을 등록하세요
