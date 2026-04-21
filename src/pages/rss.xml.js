27502d8669f2dfa2f2fe035f7c2c78dd94a1cae4
Aaron Ryu <fbcndah@gmail.com>
2026-04-21 21:57:22 +0900

    [Feat] /posts/ 로 시작하는 모든 섹션 페이지 내 글이 없을때 RSS 링크 복사 유도

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,18 +1,24 @@
+import { AARONS_BLOG_DESCRIPTION, AARONS_BLOG_TITLE } from "@/layouts/BaseLayout.astro";
 import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection("blog");
   return rss({
-    title: "Astro Learner | Blog",
-    description: "My journey learning Astro",
+    title: AARONS_BLOG_TITLE,
+    description: AARONS_BLOG_DESCRIPTION,
     site: context.site,
     // items: await pagesGlobToRssItems(import.meta.glob("@/pages/**/*.md")),
     items: posts.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      link: `/posts/${post.id}`,
+      link: `/posts/${post.slug}`,
+      content: sanitizeHtml(parser.render(post.body)), // 전체 콘텐츠 렌더링
+      customData: `<category>${post.filePath?.split("/").slice(1, -1)[0]}/${post.data.category
+        .map((each) => each.toLowerCase().replace(",", "").replace(" ", "-"))
+        .join("/")}</category>`,
+      ...post.data,
     })),
     customData: `<language>ko-kr</language>`,
   });
